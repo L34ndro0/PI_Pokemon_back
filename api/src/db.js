106595d91366e116c801c2388720709862034e9a
Leandro Carrizo <leandropuntomp3@gmail.com>
2023-05-25 16:55:03 -0300

    deploy vercel api

--- api/src/db.js
+++ api/src/db.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
+import * as pg from 'pg'
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY } = process.env;
 
 // const sequelize = new Sequelize(
@@ -17,6 +18,7 @@ const sequelize = new Sequelize(
    {
       logging: false, // set to console.log to see the raw SQL queries
       native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+      dialectModule: pg
    }
 );
 
